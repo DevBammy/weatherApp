7c3800cbb1249ae7d3cfd816dcd439e22859fe10
devBammy <devbammy@gmail.com>
2023-01-19 00:03:58 +0100

    Added features

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import "./index.scss"
 function App() {
   return (
     <>
-      <h1>My App!</h1>
+      <h1>My App!!!</h1>
       <p>Template by devBammy</p>
+      <a href="/">Call Me</a>
+      <p>NOW!</p>
     </>
   );
 }
