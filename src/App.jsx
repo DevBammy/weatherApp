f268bb46afade8d9757e6a3a1ed420f57b788c8a
devBammy <devbammy@gmail.com>
2023-01-18 23:59:01 +0100

    Starter React Template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss"
+
+function App() {
+  return (
+    <>
+      <h1>My App!</h1>
+      <p>Template by devBammy</p>
+    </>
+  );
+}
+
+export default App;
